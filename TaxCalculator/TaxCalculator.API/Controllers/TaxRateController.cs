a0d07cd8e35ad362e69e195d1336809ce64cae84
Justin Ross <jross.software@gmail.com>
2021-12-23 10:26:18 -0600

    Added API project

---
+++ TaxCalculator/TaxCalculator.API/Controllers/TaxRateController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxCalculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxRateController : ControllerBase
+    {
+        [HttpGet]
+        public double Get()
+        {
+            return 0.0;
+        }
+    }
+}
