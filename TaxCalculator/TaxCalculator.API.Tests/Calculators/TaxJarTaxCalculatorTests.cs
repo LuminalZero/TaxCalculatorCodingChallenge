a48d05e5e9623ee3b8a79877622b62886ecb4254
Justin Ross <jross.software@gmail.com>
2021-12-23 21:23:51 -0600

    Added project for tests

---
+++ TaxCalculator/TaxCalculator.API.Tests/Calculators/TaxJarTaxCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxCalculator.API.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator.API.Calculators.Tests
+{
+    [TestClass()]
+    public class TaxJarTaxCalculatorTests
+    {
+        [TestMethod()]
+        public void GetRateForLocationAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTaxForOrderAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TaxCalculator/TaxCalculator.API.Tests/Services/TaxServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxCalculator.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator.API.Services.Tests
+{
+    [TestClass()]
+    public class TaxServiceTests
+    {
+        [TestMethod()]
+        public void CalculateTaxesForOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTaxRateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
